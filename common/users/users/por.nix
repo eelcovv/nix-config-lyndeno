7ce2cee2fd9296c148bc43f0b9c3aba152d31585
Eelco van Vlietl <eelco@davelab.nl>
2025-02-02 08:00:54 +0100

    added por

---
+++ common/users/users/por.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  lib,
+  config,
+  pubKeys,
+}: let
+  keys = pubKeys.por;
+in {
+  isNormalUser = true;
+  description = "Karnrawee Mangkang";
+  uid = 1002;
+  initialPassword = "test";
+  extraGroups = [
+    "wheel"
+    "media"
+    (lib.mkIf config.networking.networkmanager.enable "networkmanager") # Do not add this group if networkmanager is not enabled
+    (lib.mkIf config.programs.adb.enable "adbusers")
+    (lib.mkIf config.programs.wireshark.enable "wireshark")
+    "libvirtd"
+    "dialout"
+    "plugdev"
+    "uaccess"
+  ];
+  shell = pkgs.fish;
+  openssh.authorizedKeys.keys = [
+    (lib.mkIf (keys ? ${config.networking.hostName}) keys.${config.networking.hostName})
+  ];
+}
---
+++ pubKeys/por.nix
@@ -0,0 +1,3 @@
+{
+  singer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEjPlzlPcny6ZKwNzdlzi85lrIhPtdjLDRov29Fbef60";
+}
