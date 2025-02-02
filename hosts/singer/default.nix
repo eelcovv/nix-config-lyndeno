579040af4fc71c10213f2ae4c70cf864c02ca0d6
Eelco van Vlietl <eelco@davelab.nl>
2025-02-02 10:47:33 +0100

    working swap

--- hosts/singer/default.nix
+++ hosts/singer/default.nix
@@ -14,6 +14,11 @@
     HibernateDelaySec=2h
   '';
 
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/7f88deec-8619-4729-b8eb-bb4f3b3fefe8"; }
+  ];
+
+
     # Enable networking
   networking.networkmanager.enable = true;
 
--- hosts/singer/swapDevices.nix
+++
@@ -1,5 +0,0 @@
-{
-[ 
-  {device = "/dev/disk/by-uuid/7f88deec-8619-4729-b8eb-bb4f3b3fefe8";}
-];
-}
