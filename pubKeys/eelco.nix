0f4b7656a98633402330401bd43ac3f66a127188
Eelco van Vlietl <eelco@davelab.nl>
2025-02-02 21:34:42 +0100

    added some programs

--- pubKeys/eelco.nix
+++ pubKeys/eelco.nix
@@ -1,4 +1,4 @@
 {
-  singer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEMUZMnnJRxNffvx2MN7KawEdOOJk5/Mx76Tkhd6EpOQ";
+  singer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAyctEDuNMzaWdkJHjeczaq5SpgkdWYwaOERpEAg2rXi";
   tongfang = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKIC8mXkA7MQ1gHJXTwysqVYOWpSjKV0IcZ+eSdNpknv";
 }
